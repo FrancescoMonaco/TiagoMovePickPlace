d2b14ce278463b6285c7aa0cdc6063193b0ad852
francescopiomonaco <francescomonaco@bitbucket.org>
2024-01-16 13:19:52 +0100

    m

--- include/group_04_a1/obstacle_finder.h
+++
@@ -1,9 +0,0 @@
-#ifndef OBSTACLE_FINDER_H
-#define OBSTACLE_FINDER_H
-
-#include <sensor_msgs/LaserScan.h>
-#include <geometry_msgs/PoseStamped.h>
-
-std::vector<geometry_msgs::PoseStamped> obstacle_finder_function(const sensor_msgs::LaserScan::ConstPtr& scan_msg);
-
-#endif
